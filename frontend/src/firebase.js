6ea8416d1652e250c191c6fe4725bb119d989851
Shivam Kumar <shivamsit846321@gmail.com>
2025-09-05 23:41:30 +0530

    Update firebase.js
    due to security resion firebase configuration removed

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,12 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDpCTyPlbHi3WsJlfbLtqeg9V7XW8OnXTM",
-  authDomain: "student-feedback-system-45c5c.firebaseapp.com",
-  projectId: "student-feedback-system-45c5c",
-  storageBucket: "student-feedback-system-45c5c.firebasestorage.app",
-  messagingSenderId: "1050471821885",
-  appId: "1:1050471821885:web:95bfeac6ddb59ee113b054"
+ // copy and past from d drive stdent feedbak management system folder containg all keys
 };
 
 const app = initializeApp(firebaseConfig);
